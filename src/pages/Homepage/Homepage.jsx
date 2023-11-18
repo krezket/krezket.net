51277c88699a6b3df24936b10e2ac73cb4423bd1
Anthony Work PC <anthonyguerrero62@outlook.com>
2023-11-18 12:10:58 -0800

    removed absolute position

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import LightDarkToggle from '../../components/LightDark/LightDarkToggle'
+import flur from '../../assets/Heraldry/flur.gif'
 import './Homepage.css'
 
 export default function Homepage(props) {
@@ -13,30 +14,37 @@ export default function Homepage(props) {
                 props.stateDark === 'dark' 
                 ? 'dark-header' 
                 : mode === 'dark' 
-                ? 'dark-header' 
-                : 'light-header' }>
+                    ? 'dark-header' 
+                    : 'light-header' }>
 
-                <div className='triangle'></div>
+                {/* <div className='triangle'></div> */}
 
                 {/* <h1 className='light-h1'>krezket</h1> */}
                 {/* <p className='light-p'>Web Design for Artists, Creators, and Musicians.</p> */}
-                
-                <h1 className={
-                    props.stateDark === 'dark' 
-                    ? 'light-h1' 
-                    : mode === 'dark' 
-                    ? 'light-h1' 
-                    : 'dark-h1'}>
 
-                    krezket
-                </h1>
+                <div className='h1-con'>
+                    <img className='flur' src={flur} alt="flur" />
+
+                    <h1 className={
+                        props.stateDark === 'dark'
+                            ? 'light-h1'
+                            : mode === 'dark'
+                                ? 'light-h1'
+                                : 'dark-h1'}>
+
+                        krezket
+                    </h1>
+
+                    <img className='flur' src={flur} alt="flur" />
+
+                </div>
 
                 <p className={
                     props.stateDark === 'dark' 
                     ? 'light-p' 
                     : mode === 'dark'
-                    ? 'light-p' 
-                    : 'dark-p'}>
+                        ? 'light-p' 
+                        : 'dark-p'}>
 
                     Web Design for Artists, Creators, and Musicians.
                 </p>
