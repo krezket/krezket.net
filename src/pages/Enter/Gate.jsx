64f04b1cd4a31e6adeb1df115d984de6ad53592b
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2023-11-20 21:23:15 -0800

    added a password to the gate

--- src/pages/Enter/Gate.jsx
+++ src/pages/Enter/Gate.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import enter from '../../assets/enter/enter-6.gif'
 import './Gate.css'
 
 export default function Gate() {
+    const navigate = useNavigate();
+    const thePassword = "deeznutz"
+    const [password, setPassword] = useState("")
+
+    const handleChange = e => {
+        if (e.target.name === "password") {
+          setPassword(e.target.value)
+        } 
+    }
+
+    const submitHandler = e => {
+        e.preventDefault()
+        password: password;
+        
+        if (password === thePassword) {
+            navigate('/home')
+        }
+        else {
+            alert("")
+        }
+    
+    }
+
     return (
         <div className='enter'>
-            <Link to='/home'>
-                <img src={enter} alt="enter" />
-            </Link>
+                <form onSubmit={submitHandler}>
+                    <input name='password' type='password' placeholder='enter password' value={password} onChange={handleChange} />
+
+                    <img src={enter} alt="enter" onClick={submitHandler} />
+                </form>
         </div>
     )
 }
