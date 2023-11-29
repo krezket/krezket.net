3bb4d395f6558614bc258f835c0adc8feae9dd8b
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2023-11-28 22:02:35 -0800

    updates

--- src/pages/Enter/Gate.jsx
+++ src/pages/Enter/Gate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import enter from '../../assets/enter/enter-6.gif'
 import './Gate.css'
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import LightDarkToggle from '../../components/LightDark/LightDarkToggle'
 // import flur from '../../assets/Heraldry/flur.gif'
