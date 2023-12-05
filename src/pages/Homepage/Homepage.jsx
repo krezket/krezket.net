c63c88424464e43025bb60339476437a7ab547d8
Anthony Work PC <anthonyguerrero62@outlook.com>
2023-12-04 22:49:58 -0800

    wizbiz, added animation to modern h1

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,9 @@
-// import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link, Element } from 'react-scroll';
-import skull from '../../assets/images/skull.jpeg';
+import wiz from '../../assets/wizbiz/wizard.gif'
 import divider1 from '../../assets/dividers/SilverGlitterBottem.gif'
 import question from '../../assets/gifs/q.gif'
-// import heraldry from '../../assets/images/krez-1-b.jpg'
-// import rose from '../../assets/backgrounds/blackrose.jpg'
 // import divider1 from '../../assets/dividers/jewel_break.gif'
 // import divider1 from '../../assets/dividers/bolt.gif'
 import './Homepage.css'
@@ -13,7 +11,16 @@ import './Homepage.css'
 export default function Homepage(props) {
     console.log(props)
     const navigate = useNavigate();
+    const [animate, setAnimate] = useState(false);
     const style = window.localStorage.getItem('style')
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+          setAnimate(true);
+        }, 500);
+    
+        return () => clearTimeout(timeout);
+      }, []);
 
     const modernToggle = e => {
         e.preventDefault()
@@ -32,10 +39,6 @@ export default function Homepage(props) {
         props.setStateModern("")
         props.setStateOldWeb("old-web")
     }
-
-    // if (code === "0") {
-    //     window.location.pathname = '/'
-    // }
 
     return (
         <>
@@ -64,7 +67,7 @@ export default function Homepage(props) {
                     <div className='mod-div'>
                         <div className='mod-con'>
                             <header className='mod-header'>
-                                <h1>KREZKET.NET</h1>
+                                <h1 className={`animated-heading ${animate ? 'animate-up' : ''}`}>KREZKET.NET</h1>
                                 <div className='style'>
                                     <button onClick={oldWebToggle}>Old Web</button>
                                     <button onClick={modernToggle}>Modern</button>
@@ -97,7 +100,7 @@ export default function Homepage(props) {
 
                     <header className='ow-header'>
 
-                        {/* <img className='heraldry' id='left' src={skull} alt="krezket" /> */}
+                        <img className='heraldry' id='left' src={wiz} alt="krezket" />
 
                         <div className='ow-h1-con'>
                             <h1 className='ow-h1'>
@@ -115,7 +118,7 @@ export default function Homepage(props) {
                             </div>
                         </div>
 
-                        {/* <img className='heraldry' id='right' src={skull} alt="krezket" /> */}
+                        <img className='heraldry' id='right' src={wiz} alt="krezket" />
 
                     </header>
 
