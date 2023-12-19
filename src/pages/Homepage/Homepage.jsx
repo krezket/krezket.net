b1aaf9a22fd46979a8ebda64d0f3b7589ddb0bdc
Anthony Work PC <anthonyguerrero62@outlook.com>
2023-12-19 07:53:03 -0800

    flipped ow and mod

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -96,88 +96,7 @@ export default function Homepage(props) {
 
     return (
         <>
-            {style === 'modern' || props.stateModern === 'modern' ?
-                <>
-                    <header className='mod-header'>
-                        <div className="mod-h1">
-                            <Link smooth={true} duration={500} to='section0' href=""><h1>KREZKET</h1></Link>
-                        </div>
-                        <h2>
-                            <Link smooth={true} duration={500} to='section1' href="">Intro</Link>
-                        </h2>
-                        <h2>
-                            <Link smooth={true} duration={500} to='section2' href="">Services</Link>
-                        </h2>
-                        <h2>
-                            <Link smooth={true} duration={500} to='section3' href="">Partners</Link>
-                        </h2>
-                        <h2>
-                            <Link smooth={true} duration={500} to='section4' href="">Clients</Link>
-                        </h2>
-                        <h2>
-                            <Link smooth={true} duration={500} to='section5' href="">Contact</Link>
-                        </h2>
-                    </header>
-
-                    <div className='mod-div'>
-                        
-                        <Element name='section0' className='mod-section-0' style={slideStyles}>
-                            <div className='style-modern'>
-                                <div className={switchOn === false ? 'style-0' : 'style-1'}>
-                                    <a onClick={oldWebToggle}>Old Web</a>
-                                </div>
-                                <button onClick={toggleSwitch}>X</button>
-                            </div>
-                            <header className='mod-title'>
-                                <h1 className={`animated-heading ${animate ? 'animate-up' : ''}`}>KREZKET.NET</h1>
-                            </header>
-                        </Element>
-
-                        <Element name='section1' className='mod-section-1'>
-                            <h2>
-                                Intro
-                            </h2>
-                            <h1>
-                                Krezket is a brand / platform for myself, Anthony Guerrero. I'm a Certified Full-Stack Web Developer with a passion for music, art, and design.
-                            </h1>
-                            <h1>
-                                I strive to be a reliable freelance web designer across a wide-range of creative media outlets.
-                            </h1>
-                        </Element>
-
-                        <Element name='section2' className='mod-section-2'>
-                            <h2>
-                                Services
-                            </h2>
-                            <h1>
-                                As a Certified Full-Stack Web Developer, I have the ability to build sites with server side functionality.
-                            </h1>
-                        </Element>
-
-                        <Element name='section3' className='mod-section-3'>
-                            <h2>
-                                Partners
-                            </h2>
-
-                        </Element>
-
-                        <Element name='section4' className='mod-section-4'>
-                            <h2>
-                                Clients
-                            </h2>
-
-                        </Element>
-
-                        <Element name='section5' className='mod-section-5'>
-                            <h2>
-                                Contact
-                            </h2>
-                            <a href="https://instagram.com/krezket" target="_blank" rel="noreferrer">Instagram</a>
-                            <a href="https://github.com/krezket" target="_blank" rel="noreferrer">GitHub</a>
-                        </Element>
-                    </div>
-                </>
-                :
+            {style === 'old-web' || props.stateModern === 'old-web' ?
                 <div className='ow-div'>
 
                     <header className='ow-header'>
@@ -251,6 +170,89 @@ export default function Homepage(props) {
 
                     </main>
                 </div>
+
+                :
+                <>
+                    <header className='mod-header'>
+                        <div className="mod-h1">
+                            <Link smooth={true} duration={500} to='section0' href=""><h1>KREZKET</h1></Link>
+                        </div>
+                        <h2>
+                            <Link smooth={true} duration={500} to='section1' href="">Intro</Link>
+                        </h2>
+                        <h2>
+                            <Link smooth={true} duration={500} to='section2' href="">Services</Link>
+                        </h2>
+                        <h2>
+                            <Link smooth={true} duration={500} to='section3' href="">Partners</Link>
+                        </h2>
+                        <h2>
+                            <Link smooth={true} duration={500} to='section4' href="">Clients</Link>
+                        </h2>
+                        <h2>
+                            <Link smooth={true} duration={500} to='section5' href="">Contact</Link>
+                        </h2>
+                    </header>
+
+                    <div className='mod-div'>
+
+                        <Element name='section0' className='mod-section-0' style={slideStyles}>
+                            <div className='style-modern'>
+                                <div className={switchOn === false ? 'style-0' : 'style-1'}>
+                                    <a onClick={oldWebToggle}>Old Web</a>
+                                </div>
+                                <button onClick={toggleSwitch}>X</button>
+                            </div>
+                            <header className='mod-title'>
+                                <h1 className={`animated-heading ${animate ? 'animate-up' : ''}`}>KREZKET.NET</h1>
+                            </header>
+                        </Element>
+
+                        <Element name='section1' className='mod-section-1'>
+                            <h2>
+                                Intro
+                            </h2>
+                            <h1>
+                                Krezket is a brand / platform for myself, Anthony Guerrero. I'm a Certified Full-Stack Web Developer with a passion for music, art, and design.
+                            </h1>
+                            <h1>
+                                I strive to be a reliable freelance web designer across a wide-range of creative media outlets.
+                            </h1>
+                        </Element>
+
+                        <Element name='section2' className='mod-section-2'>
+                            <h2>
+                                Services
+                            </h2>
+                            <h1>
+                                As a Certified Full-Stack Web Developer, I have the ability to build sites with server side functionality.
+                            </h1>
+                        </Element>
+
+                        <Element name='section3' className='mod-section-3'>
+                            <h2>
+                                Partners
+                            </h2>
+
+                        </Element>
+
+                        <Element name='section4' className='mod-section-4'>
+                            <h2>
+                                Clients
+                            </h2>
+
+                        </Element>
+
+                        <Element name='section5' className='mod-section-5'>
+                            <h2>
+                                Contact
+                            </h2>
+                            <a href="https://instagram.com/krezket" target="_blank" rel="noreferrer">Instagram</a>
+                            <a href="https://github.com/krezket" target="_blank" rel="noreferrer">GitHub</a>
+                        </Element>
+                    </div>
+                </>
+
             }
         </>
     )
