9cca8fc3ae930938d96ee0ff99db1ebdca505fe0
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2023-12-30 16:29:37 -0800

    enter got functionality

--- src/pages/Enter/Gate.jsx
+++ src/pages/Enter/Gate.jsx
@@ -1,15 +1,14 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import enter from '../../assets/enter/enter-6.gif'
 import enter2 from '../../assets/enter/enter-2.gif'
 import './Gate.css'
-import { useState } from 'react';
 
 export default function Gate(props) {
     const navigate = useNavigate();
 
     const [isOpen1, setIsOpen1] = useState('');
     const [isOpen2, setIsOpen2] = useState('');
-    const [id, setId] = useState('');
 
     const submitHandlerPage = e => {
         e.preventDefault()
@@ -29,7 +28,6 @@ export default function Gate(props) {
 
         setIsOpen1('')
         isOpen2 != '' ? setIsOpen2('') : setIsOpen2('open-ow')
-        // navigate('/oldweb')
     }
 
     return (
@@ -40,12 +38,14 @@ export default function Gate(props) {
             </header>
 
             <main className='e-main' >
-                <div className='card' id={isOpen1 === '' ? 'closed' : isOpen1 === 'open-mod' ? isOpen1 : '' } onClick={submitHandler}>
+                <div className='card' id={isOpen1 === '' ? 'closed' : isOpen1 === 'open-mod' ? isOpen1 : ''} onClick={submitHandler}>
                     <h3>Modern</h3>
+                    <Link src={enter} className='gif1' id={isOpen1 === '' ? 'closed' : isOpen1 === 'open-mod' ? isOpen1 : ''} to={'/modern'}></Link>
                 </div>
 
-                <div className='card' id={isOpen2 === '' ? 'closed' : isOpen2 === 'open-ow' ? isOpen2 : '' } onClick={submitHandler2}>
+                <div className='card' id={isOpen2 === '' ? 'closed' : isOpen2 === 'open-ow' ? isOpen2 : ''} onClick={submitHandler2}>
                     <h3>Old Web</h3>
+                    <Link src={enter2} className='gif2' id={isOpen1 === '' ? 'closed' : isOpen1 === 'open-mod' ? isOpen1 : ''} to={'/oldweb'}></Link>
                 </div>
             </main>
 
