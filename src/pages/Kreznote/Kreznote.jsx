b4cad30632e17bdac1044fbb1b13a2a7fbded4ad
Anthony <anthonyguerrero62@outlook.com>
2024-08-05 07:00:02 -0700

    added dark style

--- src/pages/Kreznote/Kreznote.jsx
+++ src/pages/Kreznote/Kreznote.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Kreznote.css'
 
 export default function Kreznote() {
+
+  useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
   return (
     <main className='k-main'>
         <h1>Kreznote</h1>
