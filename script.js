a2536e777f049785b5594d7d0016f9502a102125
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-09-03 06:43:42 -0700

    if statement in js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY || window.pageYOffset;
     console.log('Scroll Position:', scrollPosition);
+    if (scrollPosition > 300) {
+        console.log("New header")
+    }
 });
+
