4fec60df56d0334dca69b39ad29b48375e7aaea9
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-09-05 16:54:15 -0700

    new backgrounds

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,7 +1,7 @@
 let staticHeader = document.querySelector('.glass-header-inactive');
 let staticContainer= document.querySelector('.glass-container');
-let nav1 = document.querySelector('.nav-1');
-let nav2 = document.querySelector('.nav-2');
+let nav1 = document.getElementById('nav-1');
+let nav2 = document.getElementById('nav-2');
 
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY || window.pageYOffset;
@@ -9,14 +9,14 @@ window.addEventListener('scroll', function() {
     if (scrollPosition > 175) {
         staticHeader.className = 'glass-header-active';
         staticContainer.className = 'glass-container-inactive';
-        nav1.className = 'nav-1-inactive';
-        nav2.className = 'nav-2-inactive';
+        nav1.id= 'nav-1-inactive';
+        nav2.id= 'nav-2-inactive';
     }
     else if (scrollPosition < 130) {
         staticHeader.className = 'glass-header-inactive';
         staticContainer.className = 'glass-container';
-        nav1.className = 'nav-1';
-        nav2.className = 'nav-2';
+        nav1.id= 'nav-1';
+        nav2.id= 'nav-2';
     }
 });
 
