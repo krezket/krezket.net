5f61ef42539670a303f15ab60815c1a753233c16
Anthony Guerrero <anthonyguerrero62@gmail.com>
2024-09-10 14:31:12 -0700

    added text and info

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,10 +1,10 @@
 const logo = document.querySelector(".logo");
 
-const projects = document.querySelector(".projects");
-const projects2 = document.querySelector(".projects-2");
-
 const services = document.querySelector(".services");
 const services2 = document.querySelector(".services-2");
+
+const projects = document.querySelector(".projects");
+const projects2 = document.querySelector(".projects-2");
 
 const contact = document.querySelector(".contact");
 const contact2 = document.querySelector(".contact-2");
@@ -12,8 +12,8 @@ const contact2 = document.querySelector(".contact-2");
 const about = document.querySelector(".about");
 const about2 = document.querySelector(".about-2");
 
-const sectionP = document.getElementById("section-1"); 
-const sectionS = document.getElementById("section-2"); 
+const sectionS = document.getElementById("section-1"); 
+const sectionP = document.getElementById("section-2"); 
 const sectionC = document.getElementById("section-3"); 
 const sectionA = document.getElementById("section-4");
 
@@ -21,18 +21,18 @@ logo.addEventListener("click", () => {
     scrollTo(0,0)
 });
 
-projects.addEventListener("click", () => {
-    sectionP.scrollIntoView({ behavior: 'smooth' });
-});
-projects2.addEventListener("click", () => {
-    sectionP.scrollIntoView({ behavior: 'smooth' });
-});
-
 services.addEventListener("click", () => {
     sectionS.scrollIntoView({ behavior: 'smooth' });
 });
 services2.addEventListener("click", () => {
     sectionS.scrollIntoView({ behavior: 'smooth' });
+});
+
+projects.addEventListener("click", () => {
+    sectionP.scrollIntoView({ behavior: 'smooth' });
+});
+projects2.addEventListener("click", () => {
+    sectionP.scrollIntoView({ behavior: 'smooth' });
 });
 
 contact.addEventListener("click", () => {
